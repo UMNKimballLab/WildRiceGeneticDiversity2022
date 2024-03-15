16cd9e854cf689aaadc5c5a0294224dca3a4c8e1
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-03-15 08:19:26 -0600

    Add files via upload

---
+++ pop_gen_analyses/Mantel_Test/genlight_mantel.R
@@ -0,0 +1,25 @@
+library(vcfR)
+library(poppr)
+library(ape)
+
+allvcf<-read.vcfR("~/Genetic_Diversity/notemporal.vcf")
+
+samp<-read.csv("/panfs/jay/groups/21/jkimball/garbe047/Genetic_Diversity/SampleKey_Notemporal_240205.csv")
+
+#Change Sample_ID of Cultivated samples to NCROC instead of genotype name
+
+samp2<-samp
+samp2$Sample_ID<-ifelse(samp2$Class == "Breeding line", "NCROC", samp2$Sample_ID)
+
+#Make sure number of samples match up
+all(colnames(allvcf@gt)[-1] == samp2$sample_ID)
+
+#Make genlight object
+gl.mantel <- vcfR2genlight(allvcf)
+ploidy(gl.mantel) <- 2
+
+#Add population data from sample key
+pop(gl.mantel) <- samp2$Sample_ID
+
+
+save(gl.mantel,  file="~/Genetic_Diversity/genlight_mantel.Rdata")
