525a33d5291b632d7a310445919153659e8fc484
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-02 10:16:31 -0600

    Create scythe_mpileup.sh

---
+++ count_snps_in_genes/scythe_mpileup.sh
@@ -0,0 +1,29 @@
+module load htslib
+module load parallel
+
+export bams="190910_sorted_bam_files_no_JohnsonDora.txt"
+export prefix="210306_samtools"
+export ref="/home/jkimball/shared/WR_Annotation/NCBI_submission/zizania_palustris_13Nov2018_okGsv_renamedNCBI2.fasta"
+
+parallel_samtools_processes=15
+
+mkdir -p $prefix
+cut -f 1 ${ref}.fai
+
+
+scythe_mpileup() {
+        REGIONS=${1}
+        SCAFFOLD=$(echo ${REGIONS} | cut -f 1 -d ";")
+        samtools mpileup -q 20 -gDVu \
+                -b $bams \
+                -r ${REGIONS} \
+                -f ${ref} \
+                | bcftools call -mv \
+                | bgzip -c \
+                > $prefix/${prefix}_${SCAFFOLD}.vcf.gz \
+                2> $prefix/${prefix}_${SCAFFOLD}.err
+}
+
+export -f scythe_mpileup
+
+parallel --will-cite -j $parallel_samtools_processes scythe_mpileup ::: $(cut -f 1 ${ref}.fai)
