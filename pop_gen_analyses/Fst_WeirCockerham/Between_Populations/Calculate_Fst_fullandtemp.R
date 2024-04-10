ed456766928266e5625d079672d0b45d1fcd58fa
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-10 12:18:16 -0600

    Update and rename pop_gen_analyses/Fst_WeirCockerham/Calculate_Fst_fullandtemp.Rmd to pop_gen_analyses/Fst_WeirCockerham/Between_Populations/Calculate_Fst_fullandtemp.R

--- pop_gen_analyses/Fst_WeirCockerham/Between_Populations/Calculate_Fst_fullandtemp.R
+++ pop_gen_analyses/Fst_WeirCockerham/Between_Populations/Calculate_Fst_fullandtemp.R
@@ -1,41 +1,23 @@
----
-title: "Calculate fst all"
-author: "Lillian"
-date: "2024-02-05"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
-```{r}
-setwd("G:/My Drive/Kimball lab/Projects/Genetic Diversity/Fst")
 library(StAMPP)
 library(tidyverse)
 library(ggplot2)
 
 #Load genlight objects created previously
-load("G:/My Drive/Kimball lab/Projects/Genetic Diversity/Fst/Fstgenlight.Rdata")
-load("G:/My Drive/Kimball lab/Projects/Genetic Diversity/UPGMA tree/genlight_temp.Rdata")
-tempsamp<-read.csv("G:/My Drive/Kimball lab/Projects/Genetic Diversity/SampleKey_OnlyTemporal_240205.csv")
-```
+load("Fstgenlight.Rdata")
+load("genlight_temp.Rdata")
+tempsamp<-read.csv("SampleKey_OnlyTemporal_240205.csv")
 
-```{r}
 allfst<-stamppFst(gl.fstvcf, nboots=1000, percent=95, nclusters=1 )
 tempfst<-stamppFst(gl.tempvcf, nboots=1000, percent=95, nclusters=1)
-save(allfst,tempfst, file="G:/My Drive/Kimball lab/Projects/Genetic Diversity/Fst/fstscores.Rdata")
-```
-```{r}
+save(allfst,tempfst, file="fstscores.Rdata")
+
 #I exported the all fst values and organized them manually into a table for easier plotting, loading that in here
-fstdat<-read.csv("G:/My Drive/Kimball lab/Projects/Genetic Diversity/Fst/allfst_table.csv")
+fstdat<-read.csv("allfst_table.csv")
 
 #Put in the order that I want
-
 fstdat$Pop1=factor(fstdat$Pop1, levels=c("Z. aquatica" , "Bass Lake", "Clearwater River","Dahler Lake","Decker Lake","Garfield Lake", "Mud Hen Lake","Necktie River", "Ottertail River", "Phantom Lake","Lake Plantagenet", "Shell Lake", "Upper Rice Lake","K2","Barron", "Itasca-C12", "Itasca-C20", "FY-C20","PM3E"))
 fstdat$Pop2=factor(fstdat$Pop2, levels=c("Z. aquatica" , "Bass Lake", "Clearwater River","Dahler Lake","Decker Lake","Garfield Lake", "Mud Hen Lake","Necktie River", "Ottertail River", "Phantom Lake","Lake Plantagenet", "Shell Lake", "Upper Rice Lake","K2","Barron", "Itasca-C12", "Itasca-C20", "FY-C20","PM3E")) 
-```
-```{r}
+
 #Format  temp data as tibble
 tempfsttib <-
   tempfst$Fsts %>%
@@ -45,14 +27,13 @@ temppvalstib<-
   tempfst$Pvalues %>%
   as_tibble(rownames="pop1")%>%
   gather(colnames(tempfst$Pvalues), key="pop2", value="pval")
-```
-```{r}
+
+#heatmap of all samples
 heatmap<-ggplot(aes(Pop2, Pop1), data=fstdat)+geom_tile(aes(fill=Fst), color="black")+geom_text(size= 2.5, aes(label=sprintf("%0.2f", round(Fst, digits = 2))))+scale_fill_gradient("Fst", low="white", high="#7b77b6")+ theme(axis.text.x=element_text(angle=45,hjust=1,vjust=1), axis.title=element_blank(), panel.background=element_rect(fill="white"), axis.line = element_line())
 heatmap
 ggsave("all_fst_heatmap_240212.png", heatmap, width=12, height=10)
 ggsave("all_fst_heatmap_240212.tiff", heatmap, width=12, height=10)
-```
-```{r}
+
 #heatmap of temp samples
 heatmaptemp<-ggplot(aes(pop1, pop2), data=na.omit(tempfsttib))+geom_tile(aes(fill=fst), color="black")+geom_text(size= 2.5, aes(label=sprintf("%0.2f", round(fst, digits = 2))))+scale_fill_gradient("Fst", low="white", high="#7b77b6")+ theme(axis.text.x=element_text(angle=45,hjust=1,vjust=1), axis.title=element_blank(), panel.background=element_rect(fill="white"), axis.line = element_line())
 heatmaptemp
