072a37d97a6a3d8c9adf0e9cec4879417817fa29
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-10 10:44:47 -0600

    Update run_flagstat.sh

--- Quality_Checks/flagstat_files/run_flagstat.sh
+++ Quality_Checks/flagstat_files/run_flagstat.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --time=48:00:00
-#SBATCH --mem=30g
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=haasx092@umn.edu
-#SBATCH -p amdsmall
-#SBATCH --account=jkimball
-#SBATCH -o run_flagstat.out
-#SBATCH -e run_flagstat.err
 
-cd /home/jkimball/haasx092/main_GBS
+cd ~/main_GBS
 
 module load samtools
 
