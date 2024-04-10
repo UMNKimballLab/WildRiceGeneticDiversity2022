d15305d6c153a4ce67baf3cb37a3ecab6859bdf0
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-10 10:48:48 -0600

    Update bedtools_find_snps_in_genes.sh

--- Variant_Calling/count_snps_in_genes/bedtools_find_snps_in_genes.sh
+++ Variant_Calling/count_snps_in_genes/bedtools_find_snps_in_genes.sh
@@ -1,20 +1,6 @@
 #!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --time=24:00:00
-#!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --time=24:00:00
-#SBATCH --mem=30g
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=haasx092@umn.edu
-#SBATCH -p amdsmall
-#SBATCH --account=jkimball
-#SBATCH -o bedtools_find_snps_in_genes.out
-#SBATCH -e bedtools_find_snps_in_genes.err
 
-cd /home/jkimball/haasx092/plink_incl_nonbiallelic_snps
+cd ~/plink_incl_nonbiallelic_snps
 
 module load bedtools
 
