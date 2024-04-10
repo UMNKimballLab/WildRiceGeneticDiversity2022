1407cedbf140475027f8fb0909b2ebba838936d9
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-10 11:52:40 -0600

    Update run_AMOVA.sh

--- pop_gen_analyses/AMOVA/run_AMOVA.sh
+++ pop_gen_analyses/AMOVA/run_AMOVA.sh
@@ -1,17 +1,6 @@
 #!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --time=24:00:00
-#SBATCH --mem=60g
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=haasx092@umn.edu
-#SBATCH -p amdsmall
-#SBATCH --account=jkimball
-#SBATCH -o run_AMOVA.out
-#SBATCH -e run_AMOVA.err
 
-
-cd /home/jkimball/haasx092/AMOVA
+cd ~/AMOVA
 
 module load gcc/8.1.0
 module load udunits/2.2.26_gcc8.1.0
