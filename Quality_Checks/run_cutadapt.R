28e88771b2c8d5aebc47d91ed142f93019fff56c
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-10 10:39:22 -0600

    Update and rename run_cutadapt.sh to run_cutadapt.R

---
+++ Quality_Checks/run_cutadapt.R
@@ -0,0 +1,7 @@
+cd ~/main_GBS
+
+module load cutadapt
+
+for i in $(cat 190819_sample_list.txt); do
+cutadapt -b TCGCTGTCTCTTATACACATCT $i/${i}_concatenated.fq.gz -o $i/${i}_trimmed.fq.gz 2> $i/${i}_cutadapt.err
+done
