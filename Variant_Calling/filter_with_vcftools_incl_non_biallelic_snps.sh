f7eaeffb3eddaeab4cb4b26a10392a505b64b875
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-10 11:34:18 -0600

    Update filter_with_vcftools_incl_non_biallelic_snps.sh

--- Variant_Calling/filter_with_vcftools_incl_non_biallelic_snps.sh
+++ Variant_Calling/filter_with_vcftools_incl_non_biallelic_snps.sh
@@ -1,19 +1,9 @@
 #!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --time=24:00:00
-#SBATCH --mem=30g
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=haasx092@umn.edu
-#SBATCH -p amdsmall
-#SBATCH --account=jkimball
-#SBATCH -o filter_vcfs_20NA_incl_nonbiallelic_snps.out
-#SBATCH -e filter_vcfs_20NA_incl_nonbiallelic_snps.err
 
 # Include path to the working directory
-cd /home/jkimball/haasx092/main_GBS/210306_samtools
+cd ~/main_GBS/210306_samtools
 
-# Filter VCF files for missing data (20%), biallelic sites only, no indels, and a depth of 4
+# Filter VCF files for missing data (20%), no indels, and a depth of 4, minor allele frequency minimum of 0.05
 for i in $(cat vcf_file_list.txt)
 do
 STEM=$(echo ${i} | cut -f 1 -d ".")
