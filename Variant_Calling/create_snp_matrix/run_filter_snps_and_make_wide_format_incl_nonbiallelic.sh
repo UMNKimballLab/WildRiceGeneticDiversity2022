bcbd93ab7b1ab83672d7a2cf7cf9bbe75c3ff872
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-10 10:49:35 -0600

    Update run_filter_snps_and_make_wide_format_incl_nonbiallelic.sh

--- Variant_Calling/create_snp_matrix/run_filter_snps_and_make_wide_format_incl_nonbiallelic.sh
+++ Variant_Calling/create_snp_matrix/run_filter_snps_and_make_wide_format_incl_nonbiallelic.sh
@@ -1,16 +1,6 @@
 #!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --time=8:00:00
-#SBATCH --mem=30g
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=haasx092@umn.edu
-#SBATCH -p amdsmall
-#SBATCH --account=jkimball
-#SBATCH -o run_filter_snps_and_make_wide_format_incl_nonbiallelic.out
-#SBATCH -e run_filter_snps_and_make_wide_format_incl_nonbiallelic.err
 
-cd /home/jkimball/haasx092/main_GBS/210306_samtools
+cd ~/main_GBS/210306_samtools
 
 module load R/3.6.0
 # args[1] is the input TSV file, args[2] is the output CSV file, and args[3] is the output Rdata file
