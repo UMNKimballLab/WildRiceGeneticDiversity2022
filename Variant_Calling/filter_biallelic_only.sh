65205b456f165860b8ee35f1e6fb73155fc0a79f
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-10 11:35:36 -0600

    Update filter_biallelic_only.sh

--- Variant_Calling/filter_biallelic_only.sh
+++ Variant_Calling/filter_biallelic_only.sh
@@ -1,17 +1,7 @@
 #!/bin/bash -l
-#SBATCH --nodes=1
-#SBATCH --ntasks=32
-#SBATCH --time=24:00:00
-#SBATCH --mem=30g
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=haasx092@umn.edu
-#SBATCH -p amdsmall
-#SBATCH --account=jkimball
-#SBATCH -o filter_for_biallelic_only_with_vcftools.out
-#SBATCH -e filter_for_biallelic_only_with_vcftools.err
 
 # Include path to the working directory
-cd /home/jkimball/haasx092/plink_incl_nonbiallelic_snps
+cd ~/plink_incl_nonbiallelic_snps
 
 # Filter VCF file (already pre-filtered, but includes non-biallelic SNPs) for biallelic sites only
 ~/vcftools/bin/vcftools --vcf original.vcf  --min-alleles 2 --max-alleles 2  --recode --recode-INFO-all --out biallelic_snps_only
