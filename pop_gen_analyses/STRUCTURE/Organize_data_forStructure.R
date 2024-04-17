d9509060d5471c7dd1878fea3e9b857f5287a524
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2024-04-17 12:53:06 -0600

    Update Organize_data_forStructure.R

--- pop_gen_analyses/STRUCTURE/Organize_data_forStructure.R
+++ pop_gen_analyses/STRUCTURE/Organize_data_forStructure.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 #loading in snp table and edited sample key with cultivated lines separated
 dat<-read.csv("210908_normalize_incl_nonbiallelic_STRUCTURE_format_PCA_subset.csv")
-csamp<-read.csv("/home/jkimball/garbe047/Genetic_Diversity/Sample_Key_with_cultivated_231003.csv", header=TRUE)
+csamp<-read.csv("Sample_Key_with_cultivated_231003.csv", header=TRUE)
 
 #Merging snp table with sample info  and creating data sets for each sorting parameter 
 
@@ -24,7 +24,6 @@ write.csv(datapc2, "231025_Structure_data_bypcoa.csv", row.names=TRUE)
 save(csamp, dat, datapc,datawat,datapc2,datawat2,PCOAorder, Waterorder, justcultord,  file="/home/jkimball/garbe047/Genetic_Diversity/231025_structuredat.Rdata")
 
 #Open in excel and save it as a text tab delimited file then create a new project and import it into STRUCTURE
-#Data appears to be set up as haploid since we don't have two allele calls so it was put into structure as haploid, 769 individuals, 1565 loci and -9 as missing value, has row of marker names and column of individual names and data has individuals in a single line. 
 #Set up a project for each of the data orders with same parameters 
-#Parameters were set to 1000 burn in and 10,000 iterations using admixture model, allele frequencies correlated - named Matthew parameters
+#Parameters were set to 1000 burn in and 10,000 iterations using admixture model, allele frequencies correlated
 #Clicked project, start a job, picked the made parameter and ran K 2-15, 3 iterations of each
