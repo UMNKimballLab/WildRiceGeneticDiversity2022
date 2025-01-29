9e62f6b2abc15bb6fd2a0878411ddf6ac91f3864
UMNKimballLab <61478696+UMNKimballLab@users.noreply.github.com>
2025-01-29 11:23:50 -0600

    Update pophelper_plotting.R

--- pop_gen_analyses/STRUCTURE/pophelper_plotting.R
+++ pop_gen_analyses/STRUCTURE/pophelper_plotting.R
@@ -13,7 +13,6 @@ glabs <- glabs %>%
 
 #Figure 3
 fig3list<-alignK(slist[1:4])
-fig3<-plotQ(fig3list, returnplot=T,exportpath=getwd(),imgoutput="join",grplab=glabs,outputfilename=c("240220_fig3"),imgtype="png", grplabangle=25, grplabsize=1.1, divtype=1, divsize=0.5, splab=c("K=2", "K=3","K=4","K=5"), grplabheight=5, grplabpos=0.5, grplabspacer=0, showlegend=TRUE)
-
-#Figure S4 
-figS4<-plotQ(slist[c(4,9,13)], returnplot=T,exportpath=getwd(),imgoutput="join",grplab=glabs,outputfilename=c("240220_figs4"),imgtype="png", grplabangle=25, grplabsize=1.1, divtype=1, divsize=0.5, splab=c("K=5", "K=10","K=14"), grplabheight=5, grplabpos=0.5, grplabspacer=0, showlegend=TRUE)
+fig3<-plotQ(fig3list, returnplot=T,exportpath=getwd(),imgoutput="join",grplab=glabs,outputfilename=c("250120_fig3"),imgtype="png", grplabangle=25, grplabsize=1.3, divtype=1, divsize=0.5, splab=c("K=2", "K=3","K=4","K=5"),splabsize=5.2, grplabheight=8, grplabpos=0.5, grplabspacer=0, showlegend=TRUE,legendkeysize=5, legendtextsize=4, height=0.8,clustercol=c("#000000", "#E69F00","#56B4E9","#009E73","#F0E442"))
+#Figure S5 
+figS5<-plotQ(slist[c(4,9,13)], returnplot=T,exportpath=getwd(),imgoutput="join",grplab=glabs,outputfilename=c("250120_figs5"),imgtype="png", grplabangle=25, grplabsize=1.3, divtype=1, divsize=0.5, splab=c("K=5", "K=10","K=14"), splabsize=5.2,grplabheight=4.6, grplabpos=0.5, grplabspacer=0, showlegend=TRUE,legendkeysize=5, legendtextsize=4, clustercol=c("#000000", "#E69F00","#56B4E9","#009E73","#F0E442","#0072B2","#D55E00","#CC79A7","#73030F","#230786","#EBF72D","#F72D44","#02440F","#08ECE0"))
